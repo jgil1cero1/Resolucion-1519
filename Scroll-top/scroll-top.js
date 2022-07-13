83b6d72c9d1cd334b87866887dd62ea36de0dac0
jgil1cero1 <97299143+jgil1cero1@users.noreply.github.com>
2022-07-13 16:08:00 -0500

    V3

--- Scroll-top/scroll-top.js
+++ Scroll-top/scroll-top.js
@@ -1,17 +1,16 @@
-var mybutton = document.getElementById("myBtn");
+$(document).ready(function(){
+    // scrool - to top btn
+    $("#s4-workspace").on("scroll", function(){
+        // scroll to top btn
+        if ($("#s4-workspace").scrollTop() >= 400) {
+            $("#scrollUp").fadeIn(200);
+        } else {
+            $("#scrollUp").fadeOut(200);
+        }
+    });
+});
 
-// When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
-
-function scrollFunction() {
-  if (document.getElementById("s4-workspace").scrollTop > 20) {
-    mybutton.style.display = "block";
-  } else {
-    mybutton.style.display = "none";
-  }
-}
-
-// When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-  document.getElementById("s4-workspace").scrollTop = 0;
-}
+// scroll - to top btn
+$("#scrollUp").on("click", function () {
+    $("#s4-workspace").animate({ scrollTop: 0 }, 200);
+});
